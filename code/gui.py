1541a682680b9efd110862ac47b75b2d4caf851e
odys1528 <jolanta.zakrzewska@orange.pl>
2018-03-09 12:09:36 +0100

    set window

--- code/gui.py
+++ code/gui.py
@@ -1,1 +1,15 @@
-# tu będą rzeczy
+from PyQt5.QtGui import *
+import sys
+
+
+def setWindow():
+	app = QGuiApplication(sys.argv)
+	window = QWindow()
+	window.resize(800, 500)
+	window.setTitle("Tomograph Simulator")
+	window.show()
+	sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+	setWindow()
