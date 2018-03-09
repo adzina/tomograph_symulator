4dea1545f786ad66b81aca2f75ca179fc48b0bf5
odys1528 <jolanta.zakrzewska@orange.pl>
2018-03-09 16:41:27 +0100

    params test, items organised, extra gui view in png file

--- code/gui.py
+++ code/gui.py
@@ -1,8 +1,10 @@
 import sys
 import matplotlib.pyplot as plt
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QFileDialog, QLabel, QHBoxLayout
-from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import pyqtSlot, Qt
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtGui
 
 FILENAME = ""
 
@@ -30,6 +32,28 @@ class App(QWidget):
 
 		self.imageLabel = QLabel(self)
 
+		param_tree = (
+			{'name': 'img_size', 'type': 'int', 'value': 400},
+			{'name': 'n_angles', 'type': 'int', 'value': 50},
+			{'name': 'n_detectors', 'type': 'int', 'value': 10},
+			{'name': 'width', 'type': 'float', 'value': 0.9},
+			{'name': 'mask_size', 'type': 'float', 'value': 5},
+			{'name': 'play_rate', 'type': 'int', 'value': 2}
+		)
+		self.parameters = pg.parametertree.Parameter.create(name='Settings', type='group', children=param_tree)
+		self.param_tree = pg.parametertree.ParameterTree()
+		self.param_tree.setParameters(self.parameters, showTop=False)
+		self.param_tree.setSizePolicy(QtGui.QSizePolicy(QtGui.QSizePolicy.Ignored, QtGui.QSizePolicy.Preferred))
+
+		self.layout = QtGui.QVBoxLayout(self)
+		self.inputLayout = QtGui.QHBoxLayout()
+
+		self.layout.addWidget(self.loadButton)
+		self.inputLayout.addWidget(self.param_tree)
+		self.inputLayout.addWidget(self.imageLabel)
+
+		self.layout.addLayout(self.inputLayout)
+
 		self.show()
 
 	@pyqtSlot()
@@ -41,14 +65,13 @@ class App(QWidget):
 		print(FILENAME)
 
 		px = QPixmap(FILENAME)
-		px = px.scaled(200, 200, Qt.KeepAspectRatio)
+		px = px.scaled(500, 500, Qt.KeepAspectRatio)
 
 		self.imageLabel.setPixmap(px)
 		self.imageLabel.setToolTip(FILENAME)
 		self.imageLabel.move(10, self.loadButton.height()+20)
-		self.imageLabel.resize(200, 200)
+		self.imageLabel.resize(500, 500)
 		self.imageLabel.show()
-
 
 
 def startApp():
