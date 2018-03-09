89682f35a302278a8991b07f09448e17ebae3d0c
odys1528 <jolanta.zakrzewska@orange.pl>
2018-03-09 12:36:56 +0100

    load button

--- code/gui.py
+++ code/gui.py
@@ -1,6 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtCore import pyqtSlot
 
 
 class App(QWidget):
@@ -17,7 +17,18 @@ class App(QWidget):
 	def initUI(self):
 		self.setWindowTitle(self.title)
 		self.setGeometry(self.left, self.top, self.width, self.height)
+
+		loadButton = QPushButton('Load image', self)
+		loadButton.setToolTip('Click here to load the image from files')
+		loadButton.resize(100, 100)
+		loadButton.move(10, 10)
+		loadButton.clicked.connect(self.loadClickAction)
+
 		self.show()
+
+	@pyqtSlot()
+	def loadClickAction(self):
+		print('load image')
 
 
 def startApp():
