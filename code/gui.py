84756df95d888d8e9a6078bed68748d4a9544bba
odys1528 <jolanta.zakrzewska@orange.pl>
2018-03-09 12:16:35 +0100

    start app window

--- code/gui.py
+++ code/gui.py
@@ -1,15 +1,30 @@
-from PyQt5.QtGui import *
 import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
 
 
-def setWindow():
-	app = QGuiApplication(sys.argv)
-	window = QWindow()
-	window.resize(800, 500)
-	window.setTitle("Tomograph Simulator")
-	window.show()
+class App(QWidget):
+
+	def __init__(self):
+		super().__init__()
+		self.title = 'Tompgraph Simulator'
+		self.left = 10
+		self.top = 10
+		self.width = 640
+		self.height = 480
+		self.initUI()
+
+	def initUI(self):
+		self.setWindowTitle(self.title)
+		self.setGeometry(self.left, self.top, self.width, self.height)
+		self.show()
+
+
+def startApp():
+	app = QApplication(sys.argv)
+	ex = App()
 	sys.exit(app.exec_())
 
 
 if __name__ == '__main__':
-	setWindow()
+	startApp()
